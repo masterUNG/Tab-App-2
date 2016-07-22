4cf863999b589d68e5369e8cc885e4c40510f4f9
masterUNG <phrombutr@gmail.com>
2016-07-22 11:41:24 +0700

    Read JSON

--- app/src/main/java/com/example/non_jid/tabapp/ListActivity.java
+++ app/src/main/java/com/example/non_jid/tabapp/ListActivity.java
@@ -1,13 +1,19 @@
 package com.example.non_jid.tabapp;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -38,13 +44,61 @@ public class ListActivity extends AppCompatActivity {
 
         //Test List View
 
-        String[] testStrings = new String[]{"test1", "test2", "test3", "test4", "test5"};
+//        String[] testStrings = new String[]{"test1", "test2", "test3", "test4", "test5"};
+//
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+//                android.R.layout.simple_list_item_1, testStrings);
+//        listView.setAdapter(adapter);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, testStrings);
-        listView.setAdapter(adapter);
+        //Call AsyncTask
+        SynShopCenter synShopCenter = new SynShopCenter(this, listView);
+        synShopCenter.execute();
 
 
     }   // Main Method
+
+    private class SynShopCenter extends AsyncTask<Void, Void, String> {
+
+        //Explicit
+        private Context context;
+        private ListView synListView;
+        private String[] shopStrings, addressStrings, promoteStrings,
+                phoneStrings, latStrings, lngStrings;
+        private static final String urlJSON = "http://swiftcodingthai.com/non/get_data_master.php";
+
+        public SynShopCenter(Context context,
+                             ListView synListView) {
+            this.context = context;
+            this.synListView = synListView;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlJSON).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+
+            } catch (Exception e) {
+                return null;
+            }
+
+        }   // doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("22JulyV1", "JSON ==> " + s);
+
+        }   // onPost
+
+    }   // SynShopCenter Class
+
 
 }   // Main Class
---
+++ app/src/main/java/com/example/non_jid/tabapp/MyAdapter.java
@@ -0,0 +1,64 @@
+package com.example.non_jid.tabapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by masterUNG on 7/22/2016 AD.
+ */
+public class MyAdapter extends BaseAdapter{
+
+    //Explicit
+    private Context context;
+    private String[] shopStrings, phoneStrings, promoteStrings;
+
+    public MyAdapter(Context context,
+                     String[] shopStrings,
+                     String[] phoneStrings,
+                     String[] promoteStrings) {
+        this.context = context;
+        this.shopStrings = shopStrings;
+        this.phoneStrings = phoneStrings;
+        this.promoteStrings = promoteStrings;
+    }
+
+    @Override
+    public int getCount() {
+        return shopStrings.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view1 = layoutInflater.inflate(R.layout.my_listview, viewGroup, false);
+
+        //Bind Widget
+        TextView shopTextView = (TextView) view1.findViewById(R.id.textView);
+        TextView phoneTextView = (TextView) view1.findViewById(R.id.textView2);
+        TextView promoteTextView = (TextView) view1.findViewById(R.id.textView4);
+
+        //Show Text
+        shopTextView.setText(shopStrings[i]);
+        phoneTextView.setText(phoneStrings[i]);
+
+        String shortPromote = promoteStrings[i].substring(0, 40) + "...";
+        promoteTextView.setText(shortPromote);
+
+        return view1;
+    }
+}   // Main Class
