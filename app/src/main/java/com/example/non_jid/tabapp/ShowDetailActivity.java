f08b0074ce992e5157bf2b643e95e8b569768710
masterUNG <phrombutr@gmail.com>
2016-07-22 14:21:30 +0700

    Create Title & Snatet

--- app/src/main/java/com/example/non_jid/tabapp/ShowDetailActivity.java
+++ app/src/main/java/com/example/non_jid/tabapp/ShowDetailActivity.java
@@ -9,6 +9,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -70,10 +71,17 @@ public class ShowDetailActivity extends FragmentActivity implements OnMapReadyCa
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        double douLat = Double.parseDouble(getIntent().getStringExtra("Lat"));
+        double douLng = Double.parseDouble(getIntent().getStringExtra("Lng"));
+
+        LatLng latLng = new LatLng(douLat, douLng);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+        mMap.addMarker(new MarkerOptions()
+        .position(latLng)
+        .icon(BitmapDescriptorFactory.fromResource(R.drawable.pin))
+        .title(shopString)
+        .snippet(phoneString));
+
     }   // onMapReady
 
 }   //Main Class
